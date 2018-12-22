b52c94502cfb111787374e2dab106ff21a9363d1
fakedanger <fakedanger3210@gmail.com>
2018-12-22 09:08:24 +0530

    first commit

---
+++ newbooks/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(book)
+admin.site.register(blog)
+admin.site.register(comment)
+admin.site.register(cart)
+admin.site.register(invoice)
+admin.site.register(Shipping)
+admin.site.register(bookauthor)
+admin.site.register(preimg)
+admin.site.register(likesonpost)
+admin.site.register(returnbook)
+admin.site.register(donate)
+admin.site.register(verify)
+admin.site.register(resetpass)
+admin.site.register(Selectadd)
